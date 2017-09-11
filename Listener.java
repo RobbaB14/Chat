a86868ec6ecd18a158390901a346b0a704f6e535
RobbaB14 <barbara.robba@gmail.com>
2017-09-11 17:05:50 +0200

    Popravek komentarjev

--- Listener.java
+++ Listener.java
@@ -3,7 +3,9 @@ import javax.swing.event.ListSelectionListener;
 
 
 public class Listener implements ListSelectionListener {
-	//'prisluškuje' spremembam. Ko se zgodi sprememba izvede spodjo fn.
+/*	Ko v seznamu kliknemo na prijavljenjega uporabnika,
+	ga shranimo v String receiver in popravimo napis pred vnosno vrstico. 
+	(to je potrebno zato, ker se seznam stalno posodablja).*/
 	    public void valueChanged(ListSelectionEvent e) {
 	    	Gui.setReceiver();
 	    	Gui.printReceiver();
